611e2137bc7550c14cf2ff9f047c1511a8a3955b
kdaily <kdaily@users.noreply.github.com>
2015-06-03 16:13:36 -0700

    move sailfish setup to software provisioner

--- python_provisioner.sh
+++ python_provisioner.sh
@@ -31,16 +31,8 @@ git clone git://github.com/chapmanb/cloudbiolinux.git
 cd cloudbiolinux
 fab -f fabfile.py -H localhost install_biolinux:flavor=ngs_pipeline_minimal
 
-## For Sailfish
-# This needs the module file at modulefiles/Sailfish/0.6.3
-cd /opt/
-wget "https://github.com/kingsfordgroup/sailfish/releases/download/v0.6.3/Sailfish-0.6.3-Linux_x86-64.tar.gz" -O Sailfish-0.6.3-Linux_x86-64.tar.gz
-tar -xvzf Sailfish-0.6.3-Linux_x86-64.tar.gz
-cd -
-
 # AWS CLI
 pip install awscli
-
 
 ## Cleanup
 rm -rf /tmp/downloaded_packages/ /tmp/*.rds
