e8b692e41d739a04ad05b5ff1e0af987961380cb
thanneer_perumal <thanneer.perumal@sagebase.org>
2015-07-21 08:24:27 -0700

    Added x2go provisioner script, modified r_provisioner to include more packages and rstudio server, modified config.json accordingly

---
+++ x2go_provisioner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+# Update centos
+yum update
+
+# Install x2goserver and sessions
+yum install --enablerepo=epel x2goserver
+yum install --enablerepo=epel x2goserver-xsession
+
+# Install GNOME desktop components
+yum -y groupinstall "Desktop" "Desktop Platform" "X Window System" "Fonts"
+
+# You can also install the following optional GUI packages.
+yum -y groupinstall "Graphical Administration Tools"
+yum -y groupinstall "Internet Browser"
+yum -y groupinstall "General Purpose Desktop"
+yum -y groupinstall "Office Suite and Productivity"
+yum -y groupinstall "Graphics Creation Tools"
+
+# Finally, if you wanted to add the K Desktop Environment (KDE).
+yum -y groupinstall kde-desktop
