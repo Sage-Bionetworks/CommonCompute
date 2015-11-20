726ee3adea3d09637223622547a787ed50881992
kdaily <kdaily@users.noreply.github.com>
2015-11-20 13:30:03 -0800

    added mesalib

--- cloudbio_provisioner.sh
+++ cloudbio_provisioner.sh
@@ -3,5 +3,8 @@ git clone git://github.com/chapmanb/cloudbiolinux.git
 
 cd cloudbiolinux
 
+# Required for rgl/BubbleTree
+yum install mesa-libGL-devel mesa-libGLU-devel
+
 fab -f fabfile.py -H localhost install_biolinux:flavor=ngs_pipeline_minimal
 cd -
