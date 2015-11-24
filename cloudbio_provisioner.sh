0a9154b920b4f2c6b0d1b3fea6652da968200cf0
kdaily <kdaily@users.noreply.github.com>
2015-11-24 11:25:03 -0800

    updated

--- cloudbio_provisioner.sh
+++ cloudbio_provisioner.sh
@@ -1,10 +1,10 @@
-pip install fabric pyyaml
 git clone git://github.com/chapmanb/cloudbiolinux.git
 
 cd cloudbiolinux
 
 # Required for rgl/BubbleTree
-yum install mesa-libGL-devel mesa-libGLU-devel
+yum install -y mesa-libGL-devel mesa-libGLU-devel
 
+module load python/2.7.10
 fab -f fabfile.py -H localhost install_biolinux:flavor=ngs_pipeline_minimal
 cd -
