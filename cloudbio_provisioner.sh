e6aeda9f17a3897a809889ecb1edea969ce42c62
kdaily <kdaily@users.noreply.github.com>
2015-11-25 11:36:11 -0800

    updated

--- cloudbio_provisioner.sh
+++ cloudbio_provisioner.sh
@@ -1,9 +1,8 @@
-git clone git://github.com/chapmanb/cloudbiolinux.git
-
-cd cloudbiolinux
-
 # Required for rgl/BubbleTree
 yum install -y mesa-libGL-devel mesa-libGLU-devel
+
+git clone git://github.com/chapmanb/cloudbiolinux.git
+cd cloudbiolinux
 
 module load python/2.7.10
 fab -f fabfile.py -H localhost install_biolinux:flavor=ngs_pipeline_minimal
