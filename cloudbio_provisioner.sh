ea647818ce085282eca2e1d3cb261cdef6914125
kdaily <kdaily@users.noreply.github.com>
2015-12-10 10:24:06 -0800

    using bcbio now

--- cloudbio_provisioner.sh
+++ cloudbio_provisioner.sh
@@ -1,9 +1,2 @@
-# Required for rgl/BubbleTree
-yum install -y mesa-libGL-devel mesa-libGLU-devel
-
-git clone git://github.com/chapmanb/cloudbiolinux.git
-cd cloudbiolinux
-
-module load python/2.7.10
-fab -f fabfile.py -H localhost install_biolinux:flavor=ngs_pipeline_minimal
-cd -
+wget https://raw.github.com/chapmanb/bcbio-nextgen/master/scripts/bcbio_nextgen_install.py
+python bcbio_nextgen_install.py /opt/bcbio --tooldir=/opt/bcbio --isolate --nodata --aligners bwa --aligners bowtie2 --aligners bowtie --distribution centos --aligners star --aligners novoalign
