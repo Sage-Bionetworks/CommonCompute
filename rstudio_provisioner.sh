74bf167d685b1c79b8c8c48d7b2211e189b22a62
Thanneer <th.vairam@gmail.com>
2016-02-14 08:06:16 -0800

    Minor modifications in rstudio provisioner

--- rstudio_provisioner.sh
+++ rstudio_provisioner.sh
@@ -2,12 +2,12 @@
 
 ## Install Rstudio server
 wget https://download2.rstudio.org/rstudio-server-rhel-0.99.878-x86_64.rpm
-sudo yum install --nogpgcheck rstudio-server-rhel-0.99.878-x86_64.rpm
+yum install --nogpgcheck rstudio-server-rhel-0.99.878-x86_64.rpm
 
 ## Copy Rstudio configuration file
 cp /home/ec2-user/rserver.conf /etc/rstudio/rserver.conf
 
 ## Restart Rstudio server
-sudo rstudio-server restart
+rstudio-server restart
 
 
