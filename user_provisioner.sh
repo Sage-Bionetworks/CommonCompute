56c98ef805b7eeed735ab027582cc91ed29fb68c
Thanneer <th.vairam@gmail.com>
2016-02-21 15:35:46 -0800

    Added myself to be an user

--- user_provisioner.sh
+++ user_provisioner.sh
@@ -1,4 +1,4 @@
-for u in kdaily larssono kkdang apratap ssieberts jguinney ; do
+for u in kdaily larssono kkdang apratap ssieberts jguinney tperumal; do
     adduser ${u};
     echo -e "$1234password5678\n$1234password5678" | passwd -f ${u} ;
 done
