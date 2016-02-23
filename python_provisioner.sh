10f244e459463ae6f48808e753ad721d75964df4
Thanneer <th.vairam@gmail.com>
2016-02-23 07:44:45 -0800

    Following changes ewre made to python_provisioner (1) Updated versions to 2.7.11 and 3.5.1 (2) Added corresponding module files (3) changed src to /home/centos (4) changed pip installs to 2.7 and 3.5

--- python_provisioner.sh
+++ python_provisioner.sh
@@ -3,7 +3,7 @@
 # Need this for module loading
 source /etc/profile.d/modules.sh
 
-cd /root/src/
+cd /home/centos/src/
 
 yum makecache
 yum install -y ed less wget
@@ -13,59 +13,61 @@ yum install -y python python-devel
 # Will require a module file
 yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
 
-wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-tar xzf Python-2.7.10.tgz
-cd Python-2.7.10
-mkdir /opt/Python-2.7.10/
-./configure --prefix=/opt/Python-2.7.10/ --enable-unicode=ucs4 --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib" && make && make altinstall
+wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
+tar xzf Python-2.7.11.tgz
+cd Python-2.7.11
+mkdir /opt/Python-2.7.11/
+./configure --prefix=/opt/Python-2.7.11/ --enable-unicode=ucs4 --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib" && make && make altinstall && make clean
 cd ..
 
-ln -s /opt/Python-2.7.10/bin/python2.7 /opt/Python-2.7.10/bin/python
+ln -s /opt/Python-2.7.11/bin/python2.7 /opt/Python-2.7.11/bin/python
+
+# Copy pre-made module files - these were moved to the instance at the beginning
+# of the provision
+cp -R /home/centos/modulefiles/* /usr/share/Modules/modulefiles/
 
 # Requires copying of the module file at beginning of provisioning
-module load python/2.7.10
+module load python/2.7.11
 
-curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | /opt/Python-2.7.10/bin/python2.7
+curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | /opt/Python-2.7.11/bin/python2.7
 
-/opt/Python-2.7.10/bin/easy_install pip
+easy_install pip
 pip2.7 install --upgrade pip
 
 pip2.7 install cython
 
 # Install numpy, using openblas
-cd /root/src
-pip2.7 install -d /root/src numpy
+cd /home/centos/src
+pip2.7 install -d /home/centos/src numpy
 tar xzf numpy-*.tar.gz
 cd numpy-*
 
 # # files uploaded by file provisioner
 # # configuration for openblas, which should be at /opt/OpenBLAS/
-cp /home/ec2-user/numpy/site.cfg .
+cp /home/centos/numpy/site.cfg .
 
 unset CPPFLAGS
 unset LDFLAGS
 python setup.py clean && python setup.py build --fcompiler=gnu95 && python setup.py install
-# python setup.py clean && python setup.py build && python setup.py install
 
-cd /root/src
+cd /home/centos/src
 rm -rf numpy-*
 
 # Install scipy, using openblas
-cd /root/src
-pip2.7 install -d /root/src scipy
+cd /home/centos/src
+pip2.7 install -d /home/centos/src scipy
 tar xzf scipy-*.tar.gz
 cd scipy-*
 
 # # files uploaded by file provisioner
 # # configuration for openblas, which should be at /opt/OpenBLAS/
-cp /home/ec2-user/numpy/site.cfg .
+cp /home/centos/numpy/site.cfg .
 
 unset CPPFLAGS
 unset LDFLAGS
 python setup.py clean && python setup.py build --fcompiler=gnu95 && python setup.py install
-# python setup.py clean && python setup.py build && python setup.py install
 
-cd /root/src
+cd /home/centos/src
 rm -rf scipy*
 
 ## Need this to get the lib; should be in the python modulefile
@@ -76,68 +78,66 @@ module unload python
 # yum -y remove pyOpenSSL
 yum -y install libffi-devel openssl-devel
 
-module load python/2.7.10
-pip2.7 install -r /home/ec2-user/python_requirements.txt
-module unload python/2.7.10
+module load python/2.7.11
+pip3.5 install -r /home/centos/python_requirements.txt
+module unload python/2.7.11
 
 # Install python 3
 # Can use a module file
-wget https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tgz
-tar xzf Python-3.4.3.tgz
-cd Python-3.4.3
-mkdir /opt/Python-3.4.3/
-./configure --prefix=/opt/Python-3.4.3/ --enable-unicode=ucs4 --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib" && \
+wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz
+tar xzf Python-3.5.1.tgz
+cd Python-3.5.1
+mkdir /opt/Python-3.5.1/
+./configure --prefix=/opt/Python-3.5.1/ --enable-unicode=ucs4 --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib" && \
     make && make altinstall
 
 cd ..
 
-ln -s /opt/Python-3.4.3/bin/python3.4 /opt/Python-3.4.3/bin/python
+ln -s /opt/Python-3.5.1/bin/python3.5 /opt/Python-3.5.1/bin/python
 
 # Requires copying of the module file at beginning of provisioning
-module load python/3.4.3
+module load python/3.5.1
 
-curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python3.4
+curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python3.5
 
-easy_install-3.4 pip
-pip3.4 install --upgrade pip
+easy_install-3.5 pip
+pip3.5 install --upgrade pip
 
-pip3.4 install cython
+pip3.5 install cython
 
 # Install numpy, using openblas
-cd /root/src
-pip3.4 install -d /root/src numpy
+cd /home/centos/src
+pip3.5 install -d /home/centos/src numpy
 tar xzf numpy-*.tar.gz
 cd numpy-*
 
-cp /home/ec2-user/numpy/site.cfg .
+cp /home/centos/numpy/site.cfg .
 
 unset CPPFLAGS
 unset LDFLAGS
 python setup.py clean && python setup.py build --fcompiler=gnu95 && python setup.py install
-# python setup.py clean && python setup.py build && python setup.py install
 
-cd /root/src
+cd /home/centos/src
 rm -rf numpy*
 
 # Install scipy, using openblas
-cd /root/src
-pip3.4 install -d /root/src scipy
+cd /home/centos/src
+pip3.5 install -d /home/centos/src scipy
 tar xzf scipy-*.tar.gz
 cd scipy-*
 
 # # files uploaded by file provisioner
 # # configuration for openblas, which should be at /opt/OpenBLAS/
-cp /home/ec2-user/numpy/site.cfg .
+cp /home/centos/numpy/site.cfg .
 
 unset CPPFLAGS
 unset LDFLAGS
 python setup.py clean && python setup.py build --fcompiler=gnu95 && python setup.py install
-# python setup.py clean && python setup.py build && python setup.py install
 
-cd /root/src
+cd /home/centos/src
 rm -rf scipy*
 
-pip3.4 install -r /home/ec2-user/python_requirements.txt
+pip3.5 install -r /home/centos/python_requirements.txt
 
 module unload python
 
