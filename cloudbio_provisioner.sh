c8827d6ecd1eea4254560ce25241b40f61b3ca06
Thanneer <th.vairam@gmail.com>
2016-02-26 15:54:35 -0800

    Minor changes to the config script

--- cloudbio_provisioner.sh
+++ cloudbio_provisioner.sh
@@ -1,3 +1,4 @@
+cd /home/centos/src
 git clone git://github.com/chapmanb/cloudbiolinux.git
 
 cd cloudbiolinux
@@ -5,6 +6,6 @@ cd cloudbiolinux
 # Required for rgl/BubbleTree
 yum install -y mesa-libGL-devel mesa-libGLU-devel
 
-module load python/2.7.10
+module load python/3.5.1
 fab -f fabfile.py -H localhost install_biolinux:flavor=ngs_pipeline_minimal
 cd -
