fc67957217df8583b059b983693adace2781edb7
Thanneer <th.vairam@gmail.com>
2016-02-29 15:35:41 -0800

    Added extra rpackages

--- rstudio_provisioner.sh
+++ rstudio_provisioner.sh
@@ -10,3 +10,9 @@ cp /home/centos/rserver.conf /etc/rstudio/rserver.conf
 ## Restart Rstudio server
 rstudio-server restart
 
+## Install additional R packages
+Rscript -e 'install.packages(c("MRCE", "vbsr", "ctv", "psych", "reshape2", "vcd", "erer", "fpc"))'
+ 
+Rscript -e 'source("http://bioconductor.org/biocLite.R") ; biocLite(pkgs=c("RDAVIDWebService", "topGO", "goseq", "GO.db", "GSVA", "org.Hs.eg.db", "edgeR", "limma", "CePa", "Biobase", "pracma", "annotate", "AnnotationDbi", "BiocInstaller", "biomaRt", "Biostrings", "edgeR", "GEOquery", "GOstats", "graph", "GSEABase", "impute", "preprocessCore", "GO.db", "ComplexHeatmap", "FDb.InfiniumMethylation.hg19"))'
+
+Rscript -e 'install.packages(c("WGCNA", "idr"))'
