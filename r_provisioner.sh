f853dc01c4728a5f4b3acc28a02f8728a3c6aaab
Thanneer <th.vairam@gmail.com>
2016-03-07 09:40:25 -0800

    Minor Modifications

--- r_provisioner.sh
+++ r_provisioner.sh
@@ -42,19 +42,19 @@ cp /home/centos/Rprofile.site /usr/local/lib64/R/etc/Rprofile.site
 cp /home/centos/little.r /etc/littler.r
 
 ## Install R packages
-Rscript -e 'install.packages(c("docopt", "devtools", "dplyr", "tidyr", "ggplot2", "reshape2", "knitr", "stringr", "readr", "plyr", "data.table", "rJava", "doParallel", "snow", "igraph", "Rcpp", "RcppEigen", "Rclusterpp", "RColorBrewer"))'
+#Rscript -e 'install.packages(c("docopt", "devtools", "dplyr", "tidyr", "ggplot2", "reshape2", "knitr", "stringr", "readr", "plyr", "data.table", "rJava", "doParallel", "snow", "igraph", "Rcpp", "RcppEigen", "Rclusterpp", "RColorBrewer"))'
 
 ## Install synapse R client
 # For R client
-Rscript -e 'install.packages(c("RJSONIO", "RCurl", "digest"))'
+#Rscript -e 'install.packages(c("RJSONIO", "RCurl", "digest"))'
 
 Rscript -e 'source("http://depot.sagebase.org/CRAN.R") ; pkgInstall(c("synapseClient"))'
 
 ## Install bioconductor packages
-Rscript -e 'source("http://www.bioconductor.org/biocLite.R") ; biocLite(c("limma", "biovizBase", "e1071", "org.Hs.eg.db", "edgeR", "AnnotationDbi", "biomaRt", "ComplexHeatmap", "FDb.InfiniumMethylation.hg19"))'
+#Rscript -e 'source("http://www.bioconductor.org/biocLite.R") ; biocLite(c("limma", "biovizBase", "e1071", "org.Hs.eg.db", "edgeR", "AnnotationDbi", "biomaRt", "ComplexHeatmap", "FDb.InfiniumMethylation.hg19"))'
 
 ## Install rGithub client
-Rscript -e 'library(devtools); install_github("brian-bot/githubr")'
+#Rscript -e 'library(devtools); install_github("brian-bot/githubr")'
 
 ## Install Rmpi
-Rscript -e 'install.packages("Rmpi", configure.args = c("--with-Rmpi-include=/usr/include/openmpi-x86_64","--with-Rmpi-libpath=/usr/lib64/openmpi/lib","--with-Rmpi-type=OPENMPI"))'
+#Rscript -e 'install.packages("Rmpi", configure.args = c("--with-Rmpi-include=/usr/include/openmpi-x86_64","--with-Rmpi-libpath=/usr/lib64/openmpi/lib","--with-Rmpi-type=OPENMPI"))'
