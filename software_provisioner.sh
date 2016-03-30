02ab649616963b8817083b2498199453822408ab
Kenneth Daily <kmdaily@gmail.com>
2016-03-30 22:25:14 +0000

    don't do yum sys update

--- software_provisioner.sh
+++ software_provisioner.sh
@@ -2,19 +2,11 @@
 
 # Provisioner for software
 
-# need to globally disable some packages, they break the install
-# in /etc/yum.conf:
-# exclude=cloud-init,git*,ganglia*
-# cp /home/centos/yum/yum.conf /etc/
-
 mkdir /root/src/
 cd /root/src/
 
-# Get rid of this stuff - might be removed in subsequent cfncluster releases
-# yum remove -y zfs-release.noarch libnvpair1.x86_64 libuutil1.x86_64 libzfs2.x86_64 libzfs2-devel.x86_64 libzpool2.x86_64 lustre.x86_64 lustre-debuginfo.x86_64 lustre-dkms.noarch lustre-osd-ldiskfs.x86_64 lustre-osd-zfs.x86_64 lustre-source.x86_64 lustre-tests.x86_64 spl.x86_64 spl-debuginfo.x86_64 spl-dkms.noarch zfs.x86_64 zfs-debuginfo.x86_64 zfs-devel.x86_64 zfs-dkms.noarch zfs-dracut.x86_64 zfs-test.x86_64
-
 yum remove -y R-core R-core-devel R-java R-java-devel
-yum update -y
+# yum update -y
 yum install -y curl libcurl libcurl-devel readline readline-devel readline-static
 
 # wget "http://downloads.sourceforge.net/project/modules/Modules/modules-3.2.10/modules-3.2.10.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmodules%2Ffiles%2F&ts=1433366084&use_mirror=iweb" -O modules-3.2.10.tar.gz
